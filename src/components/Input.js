ef84b07c6fed0c404d62b609c45b900ce0d117a7
Eduard Camilo Ortega Sanchez <codecos.info@gmail.com>
2023-04-25 14:20:48 -0500

    buttons and inputs components

---
+++ src/components/Input.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../stylesheet/Input.css'
+
+const Input = ({ input }) => (
+    <div className="input">
+        {input}
+    </div>
+);
+
+export default Input;
